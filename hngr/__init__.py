37fc8debcaa98e385be83b883a7ff718ffa2ea69
churkle <charfei70@gmail.com>
2017-11-14 11:48:11 -0600

    Installed flask

---
+++ hngr/__init__.py
@@ -0,0 +1,1 @@
+from .hngr import app
--- hngr/hngr.py
+++ hngr/hngr.py
@@ -15,3 +15,9 @@ app.config.update(dict(
     PASSWORD='default'
 ))
 app.config.from_envvar('HNGR_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
