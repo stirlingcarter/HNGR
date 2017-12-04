190a270a3975f824e7b26a0746ddacef6add8a04
churkle <charfei70@gmail.com>
2017-12-03 23:59:28 -0600

    Changed gitignore

--- app/models.py
+++ app/models.py
@@ -17,8 +17,9 @@ class User(db.Model):
     role = db.Column(db.String(255), nullable=False)
     available = db.Column(db.Boolean(), nullable=False)
     fdc = db.relationship('FoodDistributionCenter', backref='admin', lazy=True)
+    pickups = db.relationship('Pickup', backref='donor', lazy=True)
 
-    def __init__(self, username, email, password, first_name, last_name, role): 
+    def __init__(self, username, email, password, first_name, last_name, role):
         self.username = username
         self.email = email
         self.first_name = first_name
@@ -52,7 +53,7 @@ class User(db.Model):
             )
         except Exception as e:
             return e
-    
+
     @staticmethod
     def decode_auth_token(auth_token):
         try:
@@ -62,6 +63,30 @@ class User(db.Model):
             return 'Signature expired. Please log in again.'
         except jwt.InvalidTokenError:
             return 'Invalid token. Please log in again.'
+
+class Pickup(db.Model):
+    """This class represents the pickups."""
+
+    __tablename__ = 'pickups'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    available = db.Column(db.Boolean(), nullable=False)
+    donor_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+
+
+    def __init__(self):
+        self.available = False
+        self.registered_on = datetime.datetime.now()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
 
 class FoodDistributionCenter(db.Model):
     """This class represents the FDC table."""
@@ -83,7 +108,7 @@ class FoodDistributionCenter(db.Model):
         """initialize with name."""
         self.name = name
         self.address = address
-        
+
         """Placeholder user information"""
         self.admin = User(username='churkle', email='test@test.com', password='password', first_name='charlie', last_name='fei', role='volunteer')
 
