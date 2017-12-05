362ea704e1e9598be637bac848c9f43c5d0d89a9
churkle <charfei70@gmail.com>
2017-12-04 21:31:19 -0600

    Changed for server

--- instance/config.py
+++ instance/config.py
@@ -17,7 +17,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql:///test_db'
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
 
