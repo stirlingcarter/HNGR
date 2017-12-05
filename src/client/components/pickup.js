0058a1e946bf469eb79768145b8b47dc4e892b67
nhaseous <nhat.ac@gmail.com>
2017-12-04 22:24:19 -0600

    added TODO in pickup.js

--- src/client/components/pickup.js
+++ src/client/components/pickup.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text} from 'react-native';
+import { Button } from 'react-native-elements';
+
 
 export default class PickupScreen extends React.Component {
     constructor(props) {
@@ -10,7 +12,66 @@ export default class PickupScreen extends React.Component {
     render() {
         return (
             <View>
-                <Text style={styles.plainText}>Pickup</Text>
+                <Text style={styles.title}>Pickup</Text>
+
+                /* TODO: DONOR
+                * conditional render a form for submissions (instead of this button)
+                * */
+                <Button
+                    title="Post Pickup Request"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
+                /* TODO: FDC MANAGER
+                * probably render in a list
+                * */
+                <Button
+                    title="View Incoming Deliveries"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
+                /* TODO: FDC MANAGER
+                * attached to an incoming delivery item
+                * */
+                <Button
+                    title="Set Delivery Instructions"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
+                /* TODO: FDC MANAGER */
+                <Button
+                    title="Set Hours and Locations"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
+                /* TODO: VOLUNTEER
+                * render in a list
+                * */
+                <Button
+                    title="View Pickup Requests"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
+                /* TODO: VOLUNTEER
+                * attached to a pickup request item
+                * */
+                <Button
+                    title="Accept Pickup Requests"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
+                /* TODO: VOLUNTEER */
+                <Button
+                    title="Set Availability, Location, Pickup Radius"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
+                /* TODO: VOLUNTEER */
+                <Button
+                    title="Goggle Map Directions"
+                    onPress={() => this.onRegister(params.type)}
+                />
+
             </View>
         );
     }
@@ -27,5 +88,10 @@ const styles = StyleSheet.create({
     plainText: {
         fontSize: 15,
         marginBottom: 15
-    }
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        marginBottom: 80,
+    },
 });
